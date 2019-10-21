19033fd06e87e49f7c97f04848b94f852b90d495
Miruku <archangel0660@gmail.com>
2019-10-21 12:15:17 +0200

    download & convert

--- download.php
+++ download.php
@@ -1,25 +1,65 @@
 
 <?php
     session_start();
-    if(isset($_REQUEST["file"]))
+
+    /** sends a notification message and redirects to WaterMarky.php
+     * @param msg message to confirm 
+     */
+    function popMsg($msg)
     {
-        // Get parameters
-        $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
-        $filepath = "images/" . $file;
-        
+        print_r('<script type="text/javascript" language="Javascript"> 
+                confirm("'.$msg.'"); 
+                window.location.href = "WaterMarky.php";
+                </script>');
+    }
+
+    if(isset($_SESSION['preview_pic']) && is_file($_SESSION['preview_pic']))
+    {
+        $filepath = $_SESSION['preview_pic'];
+        $fileFormat = $_POST['fileFormat'];
+    
+        $supported_format = array('jpg', 'png', 'bmp', 'svg', 'pdf'); 
+        if (isset($fileFormat) && in_array($fileFormat, $supported_format))
+        {
+            // Open the image to draw a watermark
+            $image = new Imagick();
+            $image->readImage(getcwd(). "\\".$filepath);
+
+            $image->setImageFormat($fileFormat);
+
+            //sav converted pic
+            $image->writeImage(getcwd(). "/enchanted_pics/download_pic." . $image->getImageFormat());
+            $filepath = getcwd(). "/enchanted_pics/download_pic." . $image->getImageFormat();
+        }
+
+        $filepath="C:/xampp/htdocs/WaterMarky/enchanted_pics/download_pic.svg";
+        $nam =basename($filepath);
+
+
         // Process download
         if(file_exists($filepath)) 
         {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
+            switch(strtolower(substr(strrchr($filepath,'.'),1)))
+            {
+                case 'pdf': $mime = 'application/pdf'; break;
+                case 'svg': $mime = 'image/svg+xml'; break;
+                case 'bmp': $mime = 'image/bmp'; break;
+                case 'jpg': $mime = 'image/jpg'; break;
+                case 'png': $mime = 'image/png'; break;
+                default: $mime = 'application/force-download';
+            }
+            header('Pragma: public');   // required
+            header('Expires: 0');       // no cache
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Cache-Control: private',false);
+            header('Content-Type: '.$mime);
             header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($filepath));
-            flush(); // Flush system output buffer
+            header('Content-Transfer-Encoding: binary');
+            header('Content-Length: '.filesize($filepath));    // provide file size
+            //flush(); // Flush system output buffer
             readfile($filepath);
-            exit;
+            return popMsg("Successfully enchanted picture");
         }
+        return popMsg("Something went wrong :(");
 	}
 ?>
