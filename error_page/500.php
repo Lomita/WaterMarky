ba00ef607a710bfbc09f8580037752767e216176
Miruku <archangel0660@gmail.com>
2020-01-04 16:43:13 +0100

    custom error pages 404 403 500

---
+++ error_page/500.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang=CH>
+	<head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; img-src *; style-src 'unsafe-inline'; style-src-elem *">
+        <meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'; img-src *; style-src 'unsafe-inline'; style-src-elem *">
+        <meta http-equiv="X-WebKit-CSP" content="default-src 'self'; script-src 'self'; img-src *; style-src 'unsafe-inline'; style-src-elem *">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>WaterMarky | ERROR 500</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
+	<body> 
+
+    <!-- icon and title -->
+    <div class="container bg-dark text-light"> 
+        <div class="container bg-light  text-dark"> 
+        <a class="navbar-brand align-middle text-dark" href="../WaterMarky.php">
+            <img src="/WaterMarky/rsc/wizard-nav-bar.svg" width="70" height="70" class="d-inline-block align-left">
+            <h1 class="d-inline-block align-middle">WaterMarky v2.0</h1>
+        </a>
+        </div>
+    </div>
+    
+    <!-- NAV BAR -->
+    <div class="container p-4 mb-2 bg-dark  text-light"> 
+        <h1 class="text-danger" > Error 500</h1>
+        <h4> It seems like my wand run out of magic! Click the wizard to return!
+        </div>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
